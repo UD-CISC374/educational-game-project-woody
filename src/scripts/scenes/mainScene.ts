38b1bba6517cbaa9984627e4661a2d8251a9932c
Woodyhu96 <woodyhu@udel.edu>
2020-04-23 03:29:06 +0800

    alpha submit

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -10,7 +10,7 @@ export default class MainScene extends Phaser.Scene {
   create() {
     this.exampleObject = new ExampleObject(this, 0, 0);
   }
-
+//unable to contact my partner so I guess I am solo on this project
   update() {
   }
 }
