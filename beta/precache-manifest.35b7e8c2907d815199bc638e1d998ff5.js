67c12e1131c451bf88b71d83f2bde2ffa5b218d4
Woodyhu96 <woodyhu@udel.edu>
2020-04-23 22:54:14 +0800

    Updates

---
+++ beta/precache-manifest.35b7e8c2907d815199bc638e1d998ff5.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c7b110dc744add1c8adfe8d840f168",
+    "url": "/educational-phaser-game-template/betaassets/.DS_Store"
+  },
+  {
+    "revision": "aeda7809f3dd54aa53a233448b5a7b50",
+    "url": "/educational-phaser-game-template/betaassets/citations.md"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-phaser-game-template/betaassets/icons/favicon.ico"
+  },
+  {
+    "revision": "8ab30def0ad109a97117bd62db942d75",
+    "url": "/educational-phaser-game-template/betaassets/icons/icons-16.png"
+  },
+  {
+    "revision": "feee71db2bb3ba5d11b26658baf97783",
+    "url": "/educational-phaser-game-template/betaassets/icons/icons-192.png"
+  },
+  {
+    "revision": "cb71713be71d0b8820c7ea6af9c5ddc6",
+    "url": "/educational-phaser-game-template/betaassets/icons/icons-512.png"
+  },
+  {
+    "revision": "200ce541edfaa71be93a1041736d1fee",
+    "url": "/educational-phaser-game-template/betaassets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "f6be67c5c6d974d328774d1d74e0c13e",
+    "url": "/educational-phaser-game-template/betaassets/images/background.png"
+  },
+  {
+    "revision": "67c0d30cc8166c00992dd4d7c8333861",
+    "url": "/educational-phaser-game-template/betaassets/images/battery.png"
+  },
+  {
+    "revision": "acbaa6797a09dfe9bd3fb92e6efd5366",
+    "url": "/educational-phaser-game-template/betaassets/images/clothesButton.png"
+  },
+  {
+    "revision": "39a8e94681f546ca8e0ac14b2d27bd92",
+    "url": "/educational-phaser-game-template/betaassets/images/computer.png"
+  },
+  {
+    "revision": "c86ccb57be243a3881a20b169061fa2e",
+    "url": "/educational-phaser-game-template/betaassets/images/electronic.png"
+  },
+  {
+    "revision": "3fb85a04705d3345a2fb4c704a6134c1",
+    "url": "/educational-phaser-game-template/betaassets/images/harmful.png"
+  },
+  {
+    "revision": "608f506f005908717945d1e4d6bafbcc",
+    "url": "/educational-phaser-game-template/betaassets/images/home.png"
+  },
+  {
+    "revision": "2e9588e1705013e65e35a02880abd2c2",
+    "url": "/educational-phaser-game-template/betaassets/images/medical.png"
+  },
+  {
+    "revision": "a5c3dfb5f6d1ee3b6d3131c8f0132ff1",
+    "url": "/educational-phaser-game-template/betaassets/images/p-bottle.png"
+  },
+  {
+    "revision": "bb1e9266cf973de9546f2162ee987799",
+    "url": "/educational-phaser-game-template/betaassets/images/play.png"
+  },
+  {
+    "revision": "5a31b934c6fd3c33e7112f7e514c55c6",
+    "url": "/educational-phaser-game-template/betaassets/images/recycle.png"
+  },
+  {
+    "revision": "47f94d94dc2116a8a256bc57edce0cbf",
+    "url": "/educational-phaser-game-template/betaassets/images/soda.png"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-phaser-game-template/betafavicon.ico"
+  },
+  {
+    "revision": "793258fc3184467729cb",
+    "url": "/educational-phaser-game-template/betagen.main.5c8c481b5e6f60acbf70.bundle.js"
+  },
+  {
+    "revision": "8857aeb5de5dfb8b65cf",
+    "url": "/educational-phaser-game-template/betagen.vendors.a830139833da967256f6.bundle.js"
+  },
+  {
+    "revision": "e8cbc0da5ab2018bdb0b5e3085cf6014",
+    "url": "/educational-phaser-game-template/betaindex.html"
+  },
+  {
+    "revision": "003187cab0ecf7a20e343e2ba6db4a34",
+    "url": "/educational-phaser-game-template/betamanifest.json"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "/educational-phaser-game-template/betasw.js"
+  }
+]);
---
+++ beta/sw.js
@@ -0,0 +1,11 @@
+importScripts("/educational-phaser-game-template/betaprecache-manifest.35b7e8c2907d815199bc638e1d998ff5.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
