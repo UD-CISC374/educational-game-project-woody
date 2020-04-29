96b4638da177c6d3dbbf38bb0dceaaf53c54e8c1
Woodyhu96 <woodyhu@udel.edu>
2020-04-30 03:23:27 +0800

    Updates

--- alpha/sw.js
+++ alpha/sw.js
@@ -1,4 +1,4 @@
-importScripts("/educational-game-project-amelia-and-woody/alphaprecache-manifest.2d6ce96f6340516cf0f71b9650ad7fb7.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/educational-game-project-amelia-and-woody/alphaprecache-manifest.76ff33e34f6f1b4d5a3184f6b4b0c9c5.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /**
  * You should only modify this, if you know what you are doing.
